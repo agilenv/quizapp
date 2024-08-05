55f1b0adfa4dd0b3673788f371827c435ab4fea0
Facundo Lopiano <facundolopiano@gmail.com>
2024-08-04 23:07:02 -0300

    Improve question prompt to increase difficulty

--- features/quiz/domain/prompts/MultipleChoicePrompter.ts
+++ features/quiz/domain/prompts/MultipleChoicePrompter.ts
@@ -2,7 +2,7 @@ import { Prompter } from "@/features/quiz/domain/Prompter";
 
 class MultipleChoiceQuestionPrompter implements Prompter {
   public getPrompt(): string {
-    return 'Genera una pregunta de tipo "MultipleChoice" y asegúrate que sea específica sobre el artículo analizado.';
+    return 'Genera una pregunta de tipo "MultipleChoice" de dificultad de media a alta y asegúrate que sea específica sobre el artículo analizado.';
   }
 }
 
