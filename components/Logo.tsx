81fa222b48551e81329e2002e4d94614a0296ae4
Facundo Lopiano <facundolopiano@gmail.com>
2024-08-04 23:08:03 -0300

    Improve logo component

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,6 +1,6 @@
 export default function Logo() {
   return (
-    <div className={"flex flex-row items-center gap-2 text-primary"}>
+    <div className={"flex flex-row items-center text-primary"}>
       <svg
         xmlns="http://www.w3.org/2000/svg"
         width="60"
@@ -18,7 +18,7 @@ export default function Logo() {
           <path d="M3 20l1.3 -3.9c-2.324 -3.437 -1.426 -7.872 2.1 -10.374c3.526 -2.501 8.59 -2.296 11.845 .48c3.255 2.777 3.695 7.266 1.029 10.501c-2.666 3.235 -7.615 4.215 -11.574 2.293l-4.7 1" />
         </g>
       </svg>
-      <span className={"text-3xl text-gray-600 font-bold"}>quiz app</span>
+      <span className={"text-3xl text-gray-600 font-bold"}>uiz app</span>
     </div>
   );
 }
